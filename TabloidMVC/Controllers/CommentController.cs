5d19328f9c6150ae9b4eacd4d344c9f712a12fd0
Spencer Lott <spklott@gmail.com>
2023-06-30 15:08:34 -0400

    going to test arnolds code

---
+++ TabloidMVC/Controllers/CommentController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TabloidMVC.Models;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ICommentRepository _commentRepo;
+        public CommentController(ICommentRepository commentRepository)
+        {
+            _commentRepo = commentRepository;
+        }
+        // GET: CommentsController
+        public ActionResult Index(int postId)
+        {
+            List<Comment> comments = _commentRepo.GetCommentByPostId(postId);
+
+            return View();
+        }
+
+        // GET: CommentsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CommentsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CommentsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CommentsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CommentsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CommentsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CommentsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.Data.SqlClient;
+using TabloidMVC.Models;
+
+
+namespace TabloidMVC.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly IConfiguration _config;
+
+        public CommentRepository(IConfiguration config) 
+        { 
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public List<Comment> GetAllComments()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand()) 
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, 
+                               PostId, 
+                               UserProfileId, 
+                               Subject,     
+                               Content, 
+                               CreateDateTime 
+                        FROM Comment
+                        ";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    
+                    List<Comment> comments = new List<Comment>();
+                    while (reader.Read()) 
+                    {
+                        Comment comment = new Comment
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            PostId = reader.GetInt32(reader.GetOrdinal("PostId")),
+                            UserProfileId = reader.GetInt32(reader.GetOrdinal("UserProfileId")),
+                            Subject = reader.GetString(reader.GetOrdinal("Subject")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime"))
+                        };
+                    comments.Add(comment);
+                    }
+
+                    reader.Close();
+                    return comments;
+                }
+            }
+        } //End of GetAllComments
+
+        public Comment GetCommentById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, 
+                               PostId, 
+                               UserProfileId, 
+                               Subject,     
+                               Content, 
+                               CreateDateTime 
+                        FROM Comment
+                        WHERE Id = @id
+                        ";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        Comment comment = new Comment
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            PostId = reader.GetInt32(reader.GetOrdinal("PostId")),
+                            UserProfileId = reader.GetInt32(reader.GetOrdinal("UserProfileId")),
+                            Subject = reader.GetString(reader.GetOrdinal("Subject")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime"))
+                        };
+
+                        reader.Close();
+                        return comment;
+                    }
+                    else
+                    {
+                        reader.Close();
+                        return null;
+                    }
+                }
+            }
+        } //End of GetCommentById
+
+        public List<Comment> GetCommentByPostId(int postId) 
+        {
+            using (SqlConnection conn = Connection) 
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand()) 
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, 
+                               PostId, 
+                               UserProfileId, 
+                               Subject,     
+                               Content, 
+                               CreateDateTime 
+                        FROM Comment
+                        WHERE PostId = @postId
+                        ";
+
+                    cmd.Parameters.AddWithValue("postId", postId);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Comment> comments = new List<Comment>();
+
+                    while (reader.Read()) 
+                    {
+                        Comment comment = new Comment
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            PostId = reader.GetInt32(reader.GetOrdinal("PostId")),
+                            UserProfileId = reader.GetInt32(reader.GetOrdinal("UserProfileId")),
+                            Subject = reader.GetString(reader.GetOrdinal("Subject")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime"))
+                        };
+
+                        comments.Add(comment);
+                    }
+                        reader.Close();
+                        return comments;
+                }
+            }
+        } //End of GetCommentByPostId
+
+
+    }
+}
