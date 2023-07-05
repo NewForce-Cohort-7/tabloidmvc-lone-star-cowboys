73b9e3652d5d284a14bd8cb778972587c3a7bfc4
Kathleen Tyner <kathleentyner@gmail.com>
2023-07-05 15:21:24 -0400

    new main

---
+++ TabloidMVC/Controllers/UserProfileController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic;
+using System.Security.Claims;
+using TabloidMVC.Models;
+using TabloidMVC.Models.ViewModels;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    [Authorize]
+    public class ProfileController : Controller
+    {
+        private readonly IUserProfileRepository _userProfileRepository;
+
+        public ProfileController(IUserProfileRepository userProfileRepository)
+        {
+            _userProfileRepository = userProfileRepository;
+          
+        }
+
+        private int GetCurrentUserProfileId()
+        {
+            string id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return int.Parse(id);
+        }
+
+
+        //Home Page for All Posts
+        public IActionResult Index()
+        {
+            var userProfiles = _userProfileRepository.GetAllUserProfiles();
+            return View(userProfiles);
+        }
+
+        }
+
+    }
+
+
+
